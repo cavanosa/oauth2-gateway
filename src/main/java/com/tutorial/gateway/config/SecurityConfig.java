d9a4aa4f6920fdb4f92cbf1e3c6ac7061fada5d4
cavanosa <cavanosa@cavanosa.cav>
2023-12-06 00:34:07 +0100

    main

---
+++ src/main/java/com/tutorial/gateway/config/SecurityConfig.java
@@ -0,0 +1,21 @@
+package com.tutorial.gateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
+        return http
+                .authorizeExchange(exchange -> exchange
+                        .pathMatchers("/**").permitAll()
+                        .anyExchange().authenticated())
+                .oauth2Login(Customizer.withDefaults())
+                .build();
+    }
+}
