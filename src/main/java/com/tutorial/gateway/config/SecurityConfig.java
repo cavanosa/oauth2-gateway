2e2c0f9f439e07d7c1c3bedbb85bb2e0ba362eb0
cavanosa <cavanosa@cavanosa.cav>
2023-12-08 01:08:48 +0100

    logout

--- src/main/java/com/tutorial/gateway/config/SecurityConfig.java
+++ src/main/java/com/tutorial/gateway/config/SecurityConfig.java
@@ -1,13 +1,20 @@
 package com.tutorial.gateway.config;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.oauth2.client.oidc.web.server.logout.OidcClientInitiatedServerLogoutSuccessHandler;
+import org.springframework.security.oauth2.client.registration.ReactiveClientRegistrationRepository;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.security.web.server.authentication.logout.ServerLogoutSuccessHandler;
 
 @Configuration
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final ReactiveClientRegistrationRepository repository;
 
     @Bean
     SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
@@ -16,6 +23,18 @@ public class SecurityConfig {
                         .pathMatchers("/**").permitAll()
                         .anyExchange().authenticated())
                 .oauth2Login(Customizer.withDefaults())
+                .oauth2Client(Customizer.withDefaults())
+                .logout(logout -> logout.logoutUrl("/logout").logoutSuccessHandler(successHandler(repository)))
                 .build();
     }
+
+    ServerLogoutSuccessHandler successHandler(ReactiveClientRegistrationRepository repository) {
+        OidcClientInitiatedServerLogoutSuccessHandler successHandler = new OidcClientInitiatedServerLogoutSuccessHandler(repository);
+        successHandler.setPostLogoutRedirectUri("{baseUrl}/logged-out");
+        return successHandler;
+    }
+
+    /*
+    * INSERT INTO `client_post_logout_redirect_uris` (`client_id`, `post_logout_redirect_uris`) VALUES ('3', 'http://127.0.0.1:8081/logged-out')
+    * */
 }
